fef3b86aad98b07b7c413979fb5ba68a3d165468
gengxiaoming <var_1024@163.com>
2019-08-23 15:02:05 +0800

    ss

---
+++ src/components/person.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="person">
+     sdfsdf
+ </div>
+</template>
+
+<script>
+export default {
+ name:"",
+ data(){
+  return {
+  }
+ },
+ methods:{},
+ computed:{},
+ created(){},
+ mounted(){},
+ components:{},
+ watch:{},
+}
+</script>
+
+<style lang="less" scoped>
+</style>
